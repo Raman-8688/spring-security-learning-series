831b83ba644f53c5f80b114547e535309e47af66
Raman <ramanms8688@gmail.com>
2025-10-20 21:40:59 +0530

    Day 2: Added Authentication provider

---
+++ Day02-Authantication_provider/src/main/java/security/spring/security/repo/UserRepo.java
@@ -0,0 +1,10 @@
+package security.spring.security.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import security.spring.security.model.Users;
+@Repository
+public interface UserRepo extends JpaRepository<Users,Integer> {
+    Users findByUsername(String username);
+
+}
